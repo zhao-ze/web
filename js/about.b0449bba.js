4d57adc67bc3dc39cb3713b2c88b3e3b090b5353
zhao-ze <564321812@qq.com>
2023-12-03 00:28:45 +0800

    2

--- js/about.b0449bba.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkweb"]=self["webpackChunkweb"]||[]).push([[443],{833:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(89);const o={},b=(0,i.Z)(o,[["render",c]]);var f=b}}]);
-//# sourceMappingURL=about.b0449bba.js.map
