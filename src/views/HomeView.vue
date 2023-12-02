088caf20d79b08de0d0051de20f03760f3aa63d5
zhao-ze <564321812@qq.com>
2023-12-03 01:52:42 +0800

    2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    7777
+    777788
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
